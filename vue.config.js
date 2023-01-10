54042984dbc299ef6e337fd586f08762bf2d8f2f
Léa Buendé <46668755+leabuende@users.noreply.github.com>
2023-01-10 19:56:40 +0100

    correct node env

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/Personal-portfolio/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/Personal-Portfolio/" : "/",
   };
